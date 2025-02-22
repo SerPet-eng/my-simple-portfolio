c99a8ce169d8ac482a5f94180140c54318421613
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2025-02-23 00:43:28 +0800

    Styling Navbar 🛣️✨

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,39 @@
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const links = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+    },
+    {
+      name: 'About',
+      path: '/about',
+    },
+    {
+      name: 'Contact',
+      path: '/contact',
+    },
+  ];
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <p className="navbar__logo__text">CDG</p>
+      </div>
+      <ul className="navbar__links">
+        {links.map((link, index) => {
+          return (
+            <li className="navbar__items" key={index}>
+              <Link to={link.path}>{link.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </nav>
   );
 }
