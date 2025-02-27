c7e2d6bce73a16eec20d46247f80a8489e987af9
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2025-02-27 14:00:14 +0800

    Styling Contact Page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,13 @@
 export default function Form() {
   return (
-    <form>
+    <form className="contact__form">
+      <p className="contact__form__title">Contact Me</p>
       <input type="text" placeholder="Name" />
       <input type="email" placeholder="Email" />
-      <textarea placeholder="Message" />
-      <button type="submit">Send</button>
+      <textarea type="text" rows="5" cols="33" placeholder="Message" />
+      <button className="contact__form__button" type="submit">
+        Send
+      </button>
     </form>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,29 +3,37 @@ import FacebookIcon from '../assets/socials/icon-facebook.svg';
 import TwitterIcon from '../assets/socials/icon-x-twitter.svg';
 import LinkedIn from '../assets/socials/icon-linkedin.svg';
 import GithubIcon from '../assets/socials/icon-github.svg';
+import Cover from '../assets/ContactBG.png';
 
 export default function Contact() {
   return (
-    <div>
-      <div>
-        <p>Let&apos; have a chat!</p>
-        <p>Fill up the form</p>
+    <div className="contact">
+      <div
+        className="contact__header"
+        style={{
+          backgroundImage: `url(${Cover})`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <p className="contact__title__text">Let&apos; have a chat!</p>
+        <p className="contact__description">
+          Feel free to reach out to me. I&apos;m always open to new
+          opportunities.
+        </p>
       </div>
 
-      <Form />
-
-      <div>
-        <p>Contact</p>
-        <p>Email</p>
-      </div>
-
-      <div>
-        <p>Find me in:</p>
-        <div>
-          <img src={FacebookIcon} alt="facebook" />
-          <img src={TwitterIcon} alt="twitter" />
-          <img src={LinkedIn} alt="linkedin" />
-          <img src={GithubIcon} alt="github" />
+      <div className="contact__container">
+        <Form />
+        <div className="contact__social">
+          <p className="contact__social__title">Find me in:</p>
+          <div className="contact__social__icons">
+            <img src={FacebookIcon} alt="facebook" />
+            <img src={TwitterIcon} alt="twitter" />
+            <img src={LinkedIn} alt="linkedin" />
+            <img src={GithubIcon} alt="github" />
+          </div>
         </div>
       </div>
     </div>
